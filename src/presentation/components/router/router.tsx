81dae8939c88640f3e4389be0465a67b20e064be
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-02-13 18:04:28 -0300

    fix: moves global styles to index

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Login } from '@/presentation/pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '@/presentation/styles/global.scss';
 
 export function Router(): JSX.Element {
   return (
