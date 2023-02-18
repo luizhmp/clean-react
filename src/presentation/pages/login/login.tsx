52f20d0789450342e4eeb2b7666103099c3b7c52
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-02-18 17:17:11 -0300

    feat: prevent Login to call submit more than once

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -38,6 +38,10 @@ export function Login({
   ): Promise<void> {
     event.preventDefault();
 
+    if (state.isLoading) {
+      return;
+    }
+
     setState({
       ...state,
       isLoading: true,
