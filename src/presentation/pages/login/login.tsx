7b4e83bb2da605ed61445ac352726b83100ba7fc
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-03-19 12:43:46 -0300

    feat: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Styles from './login-styles.scss';
 import {
   Footer,
@@ -94,7 +95,9 @@ export function Login({
             Entrar
           </button>
 
-          <span className={Styles.link}>Criar conta</span>
+          <Link data-testid="signup" to="/signup" className={Styles.link}>
+            Criar conta
+          </Link>
 
           <FormStatus />
         </form>
