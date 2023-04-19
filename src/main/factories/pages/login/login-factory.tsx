5bb69aa79899ea519b7a1be7dd9eb82ad533bc01
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-04-19 16:22:46 -0300

    feat: change Login composition

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Login } from '@/presentation/pages';
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication';
 import { makeLoginValidation } from './login-validation-factory';
+import { makeLocalSaveAccessToken } from '../../usecases/save-access-token';
 
 export const makeLogin = (): JSX.Element => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   );
 };
